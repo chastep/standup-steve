e1d1553757ffc906b5e61cc3c3e978c5372df604
Chase Stephens <chasestephens92@gmail.com>
2018-07-25 14:47:38 -0500

    base test of standup addition

--- skills/channel_join.js
+++ skills/channel_join.js
@@ -5,9 +5,30 @@ module.exports = function(controller) {
 
   controller.on('bot_channel_join', function(bot, message) {
   	log.info('Joined channel ' + message.channel);
+  	log.verbose(message);
+
+ 		// store channel
+ 		controller.storage.channels.get(message.channel, function(err, channel) {
+ 			if (!channel) {
+ 				log.error('channel is not present - ' + err);
+
+ 				channel = {};
+ 				channel.id = messsage.channel;
+ 				channel.standups = [];
+
+ 				controller.storage.channels.save(channel, function(err, saved_channel) {
+		 			if (err) {
+		        bot.reply(message, 'I experienced an error adding the channel: ' + err);
+		        log.error(err);
+		      } else {
+		      	log.info('channel has been successfully stored - ' + saved_channel);
+		      }
+		 		})
+ 			}
+ 		});
 
     controller.studio.run(bot, 'channel_join', message.user, message.channel, message).catch(function(err) {
-    	log.error('Error: encountered an error loading onboarding script from Botkit Studio: ' + err);
+    	log.error('Botkit Studio is shot: ' + err);
     });
   });
 }
--- skills/create_standup.js
+++ skills/create_standup.js
@@ -15,9 +15,42 @@ module.exports = function(controller) {
 		log.verbose(message);
 		log.verbose(`~~~~~~~~~~~~~~~~~~`);
 
-		controller.storage.channels.get(message.channel, function(err, channel) {
-			log.verbose(channel);
-		})
+		// store channel
+ 		controller.storage.channels.get(message.channel, function(err, channel) {
+ 			if (!channel) {
+ 				log.error('channel is not present - ' + err);
+ 				// new channel object
+ 				channel = {};
+ 				channel.id = messsage.channel;
+ 				channel.standups = [];
+
+ 				controller.storage.channels.save(channel, function(err, channel) {
+		 			if (err) {
+		        bot.reply(message, 'I experienced an error adding the channel: ' + err);
+		      } else {
+		      	log.info('channel has been successfully stored - ' + channel);
+		      	log.info(channel.standups);
+		      }
+		 		});
+ 			} else {
+ 				log.info('channel is present - ' + channel);
+
+ 				newStandup = {};
+ 				newStandup.id = 'standup_' + (channel.standups.length + 1).toString();
+
+ 				channel.standups.push(newStandup);
+
+ 				controller.storage.channels.save(channel, function(err, channel) {
+ 					if (err) {
+ 						bot.reply(message, 'I experienced an error saving this channel: ' + err);
+ 						log.error(err);
+ 					} else {
+ 						log.info('channel has been successfully saved - ' + channel);
+		      	log.info(channel.standups);	
+ 					}
+ 				})
+ 			}
+ 		});
 
 	});
 
