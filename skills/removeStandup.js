90667a0dbfc0814685249764c84ea97923af3903
Chase Stephens <chasestephens92@gmail.com>
2018-10-18 15:28:53 -0500

    refactor remove standup logic to incorporate new channels class

--- skills/removeStandup.js
+++ skills/removeStandup.js
@@ -5,37 +5,26 @@
 const log = require('../logger')('custom:remove_standup:');
 const timeHelper = require('../helpers/time.js');
 const _ = require('lodash');
+const Channel = require('../repositories/channel');
 
-function removeStandup(bot, message) {
-  log.verbose(
-    `heard this request: \n`+
-    `${message.match[0]}`
-  );
+async function removeStandup(bot, message) {
+  log.verbose(`heard this request: ${message.match[0]}`);
 
-  bot.botkit.storage.channels.get(message.channel, (err, channel) => {
-    if (!channel) {
-      bot.reply(message, `I experienced an error finding this channel: ${err}`);
-      log.error('channel is not present!');
-    } else {
-      log.info('channel is present');
+  const currentChannel = await Channel.getById(bot, message.channel);
 
-      if (!_.isEmpty(channel.standup)) {
-        channel.standup = {};
-        bot.botkit.storage.channels.save(channel, (err, channel) => {
-          if (err) {
-            bot.reply(message, `I experienced an error updating this channel: ${err}`);
-            log.error(err);
-          } else {
-            bot.reply(message, 'Standup removed :thumbsup:');
-            log.info('channel standup has been removed');
-            log.info(channel);
-          }
-        });
-      } else {
-        bot.reply(message, `This channel doesn't have a standup scheduled`);
-      }
-    }
-  });
+  log.info('channel is present');
+
+  if (_.isEmpty(currentChannel.standup)) {
+    bot.reply(message, 'There\'s no standup scheduled yet.');
+  } else {
+    currentChannel.standup = {};
+
+    const updatedChannel = await Channel.save(bot, currentChannel);
+
+    bot.reply(message, 'Standup removed :thumbsup:');
+    log.info('channel standup has been removed');
+    log.info(updatedChannel);
+  }
 }
 
 function attachSkill(controller) {
